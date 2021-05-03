bfc9569ec953e8e6d5638aaf1bc710b187bf3601
Maxime SIX <maximesix91330@hotmail.fr>
2021-05-03 23:35:18 +0200

    first version

---
+++ Test_ping.ps1
@@ -0,0 +1,22 @@
+﻿
+while ($true) {
+    $complist = Get-Content "D:/liste_pc.txt"
+    $a = Get-Date
+    Write-Host $a -ForegroundColor "yellow"
+    foreach($comp in $complist){
+    	
+    	$pingtest = Test-Connection -ComputerName $comp -Quiet -Count 1 -ErrorAction SilentlyContinue
+    	if($pingtest){
+        	Write-Host $comp -NoNewline
+		Write-Host " ONLINE" -ForegroundColor "green"
+     	}
+     	else{
+        	Write-Host $comp -NoNewline
+		Write-Host " OFFLINE" -ForegroundColor "red"
+     	}
+    }
+    echo "---------------------------------------------------------------"
+    Start-Sleep -Seconds 180
+}
+
+
